350ea4b97b6f0355732ba0bfce5ce335ffc55c9f
Minjeong Kim <lona573@gmail.com>
2021-11-18 11:28:10 +0900

    Spring boot init

---
+++ smartstore_backend/src/main/java/com/ssafy/smartstore/SmartstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.smartstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartstoreBackendApplication.class, args);
+    }
+
+}
---
+++ smartstore_backend/src/test/java/com/ssafy/smartstore/SmartstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.smartstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
